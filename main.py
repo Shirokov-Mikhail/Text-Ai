e49fb6683cbe59f16cb36ec4e5cb582db2fccbe6
Misha <m9339323029@gmail.com>
2026-02-24 22:20:07 +0700

    update index.html and style.css

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ from flask import Flask, render_template, request
 from ai.EasyOCR import Ocr
 
 import numpy as np
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html', result='none', result_text='')
+    return render_template('index.html', result='none', result_text='', text='')
+
 
 @app.route('/action', methods=["POST"])
 def acrion():
@@ -14,7 +17,12 @@ def acrion():
     print(img)
     ocr = Ocr()
     preds = ocr.result(img)
-    return render_template('index.html', result='block', result_text='\n'.join(preds))
+    if preds == []:
+        return render_template('index.html', result='block', result_text=' \n '.join(preds), text='Мы не нашли текст на фото')
+
+    else:
+        return render_template('index.html', result='block', result_text=' \n '.join(preds), text='Обнаруженный текст')
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
